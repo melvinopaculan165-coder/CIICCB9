1e0eb2b21d4486b7abc4a0f39213899be0c05fac
melvinopaculan165-coder <melvinopaculan165@gmail.com>
2025-10-21 04:21:35 +0000

    day 9

---
+++ HelloWorldApp.java
@@ -0,0 +1,9 @@
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+public class HelloWorldApp { //error 1 - clas to class //error 2 Semi-colon
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!");  // Display the string.
+    }
+}
