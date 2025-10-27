bb16b64e072fe1cee6a4a92234b071927d3171b8
melvinopaculan165-coder <melvinopaculan165@gmail.com>
2025-10-27 04:22:16 +0000

    102725

--- Chapter2/Task2.java
+++ Chapter2/Task2.java
@@ -2,11 +2,19 @@ package Chapter2;
 
 public class Task2 {
     public static void main(String[] args) {
+        // Create all primitive types (except long and double)
         byte b = 0;
         short s = 3110;
+        int i = 2;
         float f = 2.0f;
-        boolean t = true;
+        char c1 = 'H';
+        char c2 = 'w';
+        boolean bool = true;
 
-        System.out.println("H" + s + " w" + b + "r1d " + f + " " + t);
+        // Concatenate into a single string
+        String output = c1 + "" + s + " " + c2 + "0r1d " + f + " " + bool;
+
+        // Print result
+        System.out.println(output);
     }
 }
